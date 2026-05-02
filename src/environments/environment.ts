08b8409419852fbcebe67287d307d20d497984b9
tutoOmar <omar-ortiz@javeriana.edu.co>
2026-05-01 20:49:36 -0500

    refactor: automate generation of multiple environment configuration files and secure sensitive credentials via .gitignore update

--- src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: false,
-  // Base API URL for local development. Use without trailing '/auth', service appends it.
-  apiAuthUrl: 'https://auth-service-production-6ae4.up.railway.app/api',
-  apiTemplateUrl: 'https://confex-backend-template-service.onrender.com/api',
-};
