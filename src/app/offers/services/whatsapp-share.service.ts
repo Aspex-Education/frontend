d61c2da8e789ba1280432d005912718ba7a7453f
Omar Ortiz  (Tuto Omar) <103438028+tutoOmar@users.noreply.github.com>
2026-05-16 22:28:04 +0000

    Refactor: corrigiendo Url wa

--- src/app/offers/services/whatsapp-share.service.ts
+++ src/app/offers/services/whatsapp-share.service.ts
@@ -6,8 +6,8 @@ import { OfferLaboral } from '../models/offer.model';
 })
 export class WhatsappShareService {
   shareOffer(offer: OfferLaboral, baseUrl: string): void {
-    const offerUrl = `${baseUrl}/ofertas/${offer.id}`;
-    const message = `*Oferta de Trabajo o busqueda de Satélite* \n\n${offer.titulo}\n *Ubicación* : ${offer.barrio}, ${offer.ciudad} - ${offer.pais}\n\nAplica aquí o mira más detalles: ${offerUrl}`;
+    const offerUrl = `${baseUrl}/oferta/${offer.id}`;
+    const message = `*Oferta de Trabajo o busqueda de Satélite* \n\n${offer.titulo}\n ${offer.descripcion}\n*Ubicación* : ${offer.barrio}, ${offer.ciudad} - ${offer.pais}\n\nAplica aquí o mira más detalles: ${offerUrl}`;
 
     const whatsappUrl = `https://wa.me/?text=${encodeURIComponent(message)}`;
     window.open(whatsappUrl, '_blank');
